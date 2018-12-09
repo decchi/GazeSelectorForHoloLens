0337ef2a45c26b4987c9498680c90cae531b33d7
decchi <idemasahiro@hotmail.co.jp>
2018-12-09 18:37:45 +0900

    initial commit

---
+++ Assets/HoloToolkitExtension/Input/GazeSelector/Script/GazeSelectorTarget.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using HoloToolkit.Unity.InputModule;
+
+namespace HoloToolkitExtension.Input
+{
+
+    public class GazeSelectorTarget : MonoBehaviour, IFocusable
+    {
+
+        public UnityEvent OnSelectionCompleted = new UnityEvent();
+
+        public void OnFocusEnter()
+        {
+            SelectionRadial.Instance.OnSelectionComplete += OnSelectionComplete;
+            SelectionRadial.Instance.Show();
+            SelectionRadial.Instance.HandleDown();
+        }
+
+        public void OnFocusExit()
+        {
+            SelectionRadial.Instance.OnSelectionComplete -= OnSelectionComplete;
+            SelectionRadial.Instance.HandleUp();
+            SelectionRadial.Instance.Hide();
+
+        }
+
+        private void OnSelectionComplete()
+        {
+            SelectionRadial.Instance.OnSelectionComplete -= OnSelectionComplete;
+            OnSelectionCompleted.Invoke();
+        }
+    }
+
+}
---
+++ Assets/HoloToolkitExtensionSample/Scripts/ColorChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour {
+
+
+
+    public void ColorChangeBlue(Renderer renderer)
+    {
+        renderer.material.color = Color.blue;
+    }
+
+    public void ColorChangeGreen(Renderer renderer)
+    {
+        renderer.material.color = Color.green;
+    }
+
+}
